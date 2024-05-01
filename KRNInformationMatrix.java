41d89c2a97d563b0bc8702f203100adc963c24c5
Vitaly Ponomarev <vp211082@gmail.com>
2024-05-01 01:09:24 -0500

    created main method in class KRNInformationMatrix

--- KRNInformationMatrix.java
+++ KRNInformationMatrix.java
@@ -1,2 +1,11 @@
+//import java.util.concurrent.TimeUnit;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.FirefoxDriver;
+
 public class KRNInformationMatrix {
+    public static void main(String[] args) {
+
+    }
+
 }
