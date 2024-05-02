65ac9d99cfbb4d1d38e7543cd69df111e51f7860
Vitaly Ponomarev <vp211082@gmail.com>
2024-05-02 03:03:42 -0500

    imported packages and checked compatibility between geckodriver and firefox

--- KRNInformationMatrix.java
+++ KRNInformationMatrix.java
@@ -1,10 +1,16 @@
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class KRNInformationMatrix {
     public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "/snap/bin/geckodriver");
+        FirefoxOptions options = new FirefoxOptions();
+        options.setHeadless(true);
+        WebDriver driver = new FirefoxDriver();
+
 
     }
 
