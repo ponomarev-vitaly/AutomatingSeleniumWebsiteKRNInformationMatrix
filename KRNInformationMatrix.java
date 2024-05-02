16d7a505a5972523404d77647b279170524124b5
Vitaly Ponomarev <vp211082@gmail.com>
2024-05-02 01:08:09 -0500

    modified project in maven project

--- KRNInformationMatrix.java
+++ KRNInformationMatrix.java
@@ -1,7 +1,7 @@
-//import java.util.concurrent.TimeUnit;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.FirefoxDriver;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.FirefoxDriver;
 
 public class KRNInformationMatrix {
     public static void main(String[] args) {
