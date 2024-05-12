6b6e7c39b7be87985201c4c8a0b98476a9164c7f
Vitaly Ponomarev <vp211082@gmail.com>
2024-05-11 21:07:48 -0500

    updated the code, dependencies, decided to check the test in Mozilla Firefox browser

--- KRNInformationMatrix.java
+++ KRNInformationMatrix.java
@@ -1,28 +1,19 @@
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-// import org.openqa.selenium.firefox.FirefoxDriver;
-// import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class KRNInformationMatrix {
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.gecko.driver", "/snap/bin/geckodriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
 
-        // options.setHeadless(true);
-        WebDriver driver = new ChromeDriver(options);
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
+        WebDriver driver = new FirefoxDriver();
 
         driver.manage().window().maximize();
         driver.get("https://krninformatix.com/selenium/testing.html");
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[11]/div[1]/p[1]/button[1]")).click();
         Thread.sleep(3000);
-        // driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[11]/div[1]/p[1]/button[1]")).click();
-        // Thread.sleep(3000);
         String altText = driver.switchTo().alert().getText();
         System.out.println(altText);
         driver.switchTo().alert().accept();
